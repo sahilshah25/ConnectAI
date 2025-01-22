9bf9d640c67693ed78e530787950bd5f9ae9799d
sahil <sahilshah84621@gmail.com>
2025-01-23 01:04:23 +0530

    this is first commit

---
+++ ConnectAI/src/main/java/com/ConnectAI/ConnectAiApplication.java
@@ -0,0 +1,13 @@
+package com.ConnectAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectAiApplication.class, args);
+	}
+
+}
---
+++ ConnectAI/src/test/java/com/ConnectAI/ConnectAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ConnectAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
